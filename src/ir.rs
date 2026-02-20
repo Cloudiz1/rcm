f9e3e7a9f782841dae0e37785601a50c4b0bda24
Hazel <vincentni2007@gmail.com>
2026-02-20 18:13:34 -0500

    GVN

--- src/ir.rs
+++ src/ir.rs
@@ -1,12 +1,12 @@
 use crate::lexer;
 use crate::parser;
-use std::borrow::BorrowMut;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
-use std::cell::RefCell;
 use std::vec::Vec;
 
 struct SSAGen {
+    blocks: Vec<Block>
     // current_definition: HashMap<String, Expression>
 }
 
@@ -44,37 +44,78 @@ struct SSADeclaration {
 #[derive(Clone)]
 struct Block {
     predecessors: Vec<Rc<RefCell<Block>>>,
-    instructions: Vec<SSADeclaration>,
-    definitions: HashMap<String, Expression>,
-    incomplete_phis: Vec<String>,
+    successors: Vec<Rc<RefCell<Block>>>,
+
+    // predecessors: Vec<usize>,
+    // successors: Vec<usize>,
+    // instructions: Vec<SSADeclaration>,
+    // TODO: i seem to need to hold a current definitions as well
+    // because LVN should return an identifier in most cases
+    
+    // definitions: HashMap<String, Expression>,
+    definitions: HashMap<String, usize>,
+    incomplete_phis: Vec<Expression>,
     sealed: bool,
 }
 
-fn write_variable(variable: Identifier, block: &mut Block, value: Expression) {
-    block.definitions.insert(variable.name, value);
-}
 
-fn read_variable_recursive(variable: Identifier, block: &mut Block) -> Expression {
-    let val;
-    if !block.sealed {
-        val = Expression::Phi { operands: Vec::new() };
-        write_variable(variable.clone(), block, val);
-        block.incomplete_phis.push(variable.name.clone());
-    } else if block.predecessors.len() == 1 {
-        // let mut predecessor_rc = Rc::clone(&block.predecessors[0]);
-        let predecessor = block.predecessors[0].borrow();
-        read_variable(variable, predecessor);
-        // val = read_variable(variable, &mut predecessor);
+
+impl SSAGen {
+    pub fn new() -> Self {
+        Self {
+            blocks: Vec::new(),
+        }
     }
-    return Expression::Int(0);
+} 
+
+fn update_variant(variable: &String, block: &mut Block) -> usize {
+    let Some(variant) = block.definitions.get(variable) else {
+        unreachable!("could not find a defininition for {}", *variable);
+    };
+
+    let new = variant + 1;
+    block.definitions.insert(variable.clone(), new);
+    return new;
 }
 
-fn read_variable(variable: Identifier, block: &mut Block) -> Expression {
-    if let Some(current) = block.definitions.get(&variable.name) {
+fn read_variable(variable: &String, block: &mut Block) -> usize {
+    if let Some(current) = block.definitions.get(variable) {
         return current.clone();
     }
 
     return read_variable_recursive(variable, block);
+}
+
+fn read_variable_recursive(variable: &String, block: &mut Block) -> usize {
+    if !block.sealed {
+        let val = Expression::Phi { operands: Vec::new() };
+        let out = update_variant(variable, block);
+        block.incomplete_phis.push(val);
+        return out;
+    } else if block.predecessors.len() == 1 {
+        let mut predecessor = (&block.predecessors[0]).borrow_mut();
+        return read_variable(variable, &mut *predecessor);
+    } else {
+        let mut phi = Expression::Phi { operands: Vec::new() };
+        let out = update_variant(variable, block);
+        add_phi_operands(variable, &mut phi, block);
+        return out;
+    }
+}
+
+fn add_phi_operands(variable: &String, phi: &mut Expression, block: &Block) {
+    let Expression::Phi { operands } = phi else {
+        unreachable!("RCM internal: can not call IR:add_phi_operands with a non-phi block input");
+    };
+
+    for predecessor in block.predecessors.iter() {
+        let mut pred = predecessor.borrow_mut();
+        let variant = read_variable(variable, &mut *pred);
+        operands.push(Identifier {
+            name: variable.clone(),
+            variant
+        });
+    }
 }
 
 pub fn generate_basic_block(block: parser::Statement) {
