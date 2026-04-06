7990b2f6b943741efe17de26f94dd23ec1f372ec
= <vincentni2007@gmail.com>
2026-04-06 11:30:47 -0400

    LVN, GVN, add_phi_operands(), most of remove_trivial_phi(), reroute(), missing a table (or just an O(n) iteration) to update current_def

--- src/ir.rs
+++ src/ir.rs
@@ -3,11 +3,13 @@ use std::collections::HashMap;
 
 pub type BlockId = usize;
 pub type ValueId = usize;
+const UNDEF_ID: usize = 0;
 
 pub struct SSA {
     blocks: Vec<Block>, // uses BlockId
     values: Vec<Value>, // uses ValueId
     types: Vec<parser::Type>, // uses ValueId
+    use_chains: Vec<Vec<ValueId>>,
 
     // track the current id for arenas
     curr_val_id: ValueId,
@@ -26,6 +28,7 @@ pub struct Block {
     pub successors: Vec<BlockId>
 }
 
+#[derive(Clone)]
 pub enum BinaryOp {
     Add,
     Sub,
@@ -46,11 +49,13 @@ pub enum BinaryOp {
     NotEq,
 }
 
+#[derive(Clone)]
 pub enum UnaryOp {
     Not, 
     Neg,
 }
 
+#[derive(Clone)]
 pub enum Value {
     Int(i64),
     Float(f64),
@@ -87,7 +92,7 @@ pub enum Value {
         if_false: BlockId,
     },
     Phi {
-        // variable: String,
+        variable: String,
         block: BlockId,
         operands: Vec<ValueId>,
     }
@@ -99,10 +104,15 @@ impl SSA {
             blocks: Vec::new(),
             values: Vec::new(),
             types: Vec::new(),
+            use_chains: Vec::new(),
 
-            curr_val_id: 0,
+            curr_val_id: UNDEF_ID + 1,
             curr_block_id: 0,
         }
+    }
+
+    fn add_use(&mut self, operand: ValueId, user: ValueId) {
+        self.use_chains[operand].push(user);
     }
 
     fn add_variable(&mut self, value: Value) -> ValueId {
@@ -113,9 +123,7 @@ impl SSA {
 
     fn write_variable(&mut self, variable: String, block: BlockId, value: ValueId) {
         let b = &mut self.blocks[block];
-        if let Some(_) = b.current_defintions.get(&variable) {
-            b.current_defintions.insert(variable, value);
-        }
+        b.current_defintions.insert(variable, value);
     }
 
     fn read_variable(&mut self, variable: String, block: BlockId) -> ValueId {
@@ -127,30 +135,95 @@ impl SSA {
     }
 
     fn read_variable_recursive(&mut self, variable: String, block: BlockId) -> ValueId {
-        let mut v: ValueId;
+        let v: ValueId;
         if !self.blocks[block].sealed {
-            let phi = Value::Phi { block: block, operands: Vec::new() };
+            let phi = Value::Phi { variable: variable.clone(), block, operands: Vec::new() };
             v = self.add_variable(phi);
-            self.blocks[block].incomplete_phis.insert(variable, v);
+            self.blocks[block].incomplete_phis.insert(variable.clone(), v);
         } else if self.blocks[block].predecessors.len() == 1 {
-            v = self.read_variable(variable, self.blocks[block].predecessors[0]);
+            v = self.read_variable(variable.clone(), self.blocks[block].predecessors[0]);
         } else {
-            let phi = Value::Phi { block: block, operands: Vec::new() };
-            v = self.add_variable(phi);
-            self.write_variable(variable, block, v);
-            v = self.add_phi_operands(variable, v);
+            let phi = Value::Phi { variable: variable.clone(), block, operands: Vec::new() };
+            v = self.add_variable(phi.clone());
+            self.write_variable(variable.clone(), block, v);
+            self.add_phi_operands(variable.clone(), v);
         }
 
         self.write_variable(variable, block, v);
         return v;
     }
 
-    fn add_phi_operands(&mut self, variable: String, phi: Value) {
-        let Value::Phi { block, operands } = phi else {
-            panic!("internal error: can not call ir::SSA::add_phi_operands() without a phi variant");
+    fn add_phi_operands(&mut self, variable: String, phi_id: ValueId) -> ValueId {
+        let block_id = match &self.values[phi_id] {
+            Value::Phi { block, .. } => *block,
+            _ => panic!("internal error: can not call ir::SSA::add_phi_operands() without a phi variant"),
         };
 
-        for pred in &self.blocks[block].predecessors {
+        let preds = self.blocks[block_id].predecessors.to_owned();
+        let mut new_operands: Vec<ValueId> = Vec::new();
+
+        for pred in preds {
+            let operand = self.read_variable(variable.clone(), pred);
+            self.add_use(operand, phi_id);
+            new_operands.push(operand);
         }
+
+        if let Value::Phi { operands, .. } = &mut self.values[phi_id] {
+            *operands = new_operands.clone();
+        }
+
+        return self.remove_trivial_phi(phi_id);
+    }
+
+    fn remove_trivial_phi(&mut self, phi_id: ValueId) -> ValueId {
+        let same = {
+            let Value::Phi { operands, ..} = &self.values[phi_id] else { return phi_id };
+            let mut same: Option<ValueId> = None;
+            for &op in operands {
+                if Some(op) == same || op == phi_id { continue };
+                if same.is_some() { return phi_id };
+                same = Some(op);
+            }
+
+            same.unwrap_or(UNDEF_ID)
+        };
+
+        let users = std::mem::take(&mut self.use_chains[phi_id]);
+        for user_id in users {
+            if user_id == phi_id { continue }
+            self.reroute(user_id, phi_id, same);
+
+            if let Value::Phi { .. } = self.values[user_id] {
+                self.remove_trivial_phi(user_id);
+            }
+        }
+
+        return same;
+    }
+
+    fn reroute(&mut self, user_id: ValueId, old: ValueId, new: ValueId) {
+        match &mut self.values[user_id] {
+            Value::Binary { lhs, rhs, ..} => {
+                if *lhs == old { *lhs = new }
+                if *rhs == old { *rhs = new }
+            },
+            Value::Phi { operands, .. } => {
+                for op in operands.iter_mut() {
+                    if *op == old { *op = new }
+                }
+            }
+            _ => unimplemented!("reroute is still a WIP"),
+        }
+
+        self.add_use(new, user_id);
+    }
+
+    fn seal_block(&mut self, block_id: BlockId) {
+        let incomplete_phis = std::mem::take(&mut self.blocks[block_id].incomplete_phis);
+        for (variable, phi) in incomplete_phis {
+            self.add_phi_operands(variable, phi);
+        }
+
+        self.blocks[block_id].sealed = true;
     }
 }
